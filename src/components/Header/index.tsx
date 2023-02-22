242f803f5187872e9f5f5a4e20ac203fa01eaece
allanlalangan <allanlalangan@gmail.com>
2023-02-22 14:17:41 -0800

    create Header and Sidebar components add grid styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="col-span-12 flex items-center justify-between bg-slate-800 px-4 py-2">
+      <p>Header</p>
+    </header>
+  );
+};
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+  return (
+    <aside className="col-span-3 flex flex-col items-center justify-between bg-slate-700 px-4 py-2">
+      <p>Sidebar</p>
+    </aside>
+  );
+};
